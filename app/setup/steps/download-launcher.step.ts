280b0b76d0f44f785311aeac8bf09a255fd542d5
FoxSGR <foxsgrno1@gmail.com>
2023-03-18 15:06:34 +0000

    feat: icons
    Took 2 minutes

--- app/setup/steps/download-launcher.step.ts
+++ app/setup/steps/download-launcher.step.ts
@@ -3,8 +3,26 @@ import path from 'path';
 import { log } from 'electron-log';
 
 import { SetupStepConfig } from '../types';
-import { DATA_DIR, LAUNCHER_DIR, LAUNCHER_URL } from '../const';
+import { DATA_DIR, LAUNCHER_DIR, LAUNCHER_EXECUTABLE } from '../const';
 import { downloadFile, extract } from '../../util';
+
+/**
+ * The URL to download the launcher from.
+ */
+export let LAUNCHER_URL =
+	process.env['LAUNCHER_URL'] ||
+	'https://nightly.link/UltimMC/Launcher/workflows/main/develop/mmc-cracked-win32.zip';
+if (process.platform === 'linux') {
+	LAUNCHER_URL = LAUNCHER_URL.replace('win32', 'lin64');
+	if (LAUNCHER_URL.includes('.multimc.')) {
+		LAUNCHER_URL = LAUNCHER_URL.replace('.zip', '.tar.gz');
+	}
+} else {
+	LAUNCHER_URL = LAUNCHER_URL.replace('lin64', 'win32').replace(
+		'.tar.gz',
+		'.zip',
+	);
+}
 
 /**
  * This step downloads and extracts the backend launcher.
@@ -22,6 +40,11 @@ export const downloadLauncherStep: SetupStepConfig = {
 		const fileName = 'mmc.zip';
 		const zipDir = path.join(DATA_DIR, fileName);
 		await downloadFile(LAUNCHER_URL, zipDir, event);
-		await extract(zipDir, LAUNCHER_DIR, true, true);
+		await extract(zipDir, LAUNCHER_DIR, true);
+
+		// Give write permission to the file
+		if (process.platform === 'linux') {
+			await fs.promises.chmod(LAUNCHER_EXECUTABLE, 0o755);
+		}
 	},
 };
